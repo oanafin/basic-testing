67ca6ddd0f5719baae6024208a4ea0b70f2ef2e6
oanafin <olzhas.anafin@gmail.com>
2024-10-20 02:24:32 +0500

    feat: implement 08-snapshot-testing

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,42 @@
 // Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
+import { describe, test, expect } from '@jest/globals';
 
 describe('generateLinkedList', () => {
   // Check match by expect(...).toStrictEqual(...)
-  test('should generate linked list from values 1', () => {
-    // Write your test here
+  test('should generate linked list from array [1, 2, 3]', () => {
+    const input = [1, 2, 3];
+    const expectedOutput = {
+      value: 1,
+      next: {
+        value: 2,
+        next: {
+          value: 3,
+          next: {
+            value: null,
+            next: null,
+          },
+        },
+      },
+    };
+
+    expect(generateLinkedList(input)).toStrictEqual(expectedOutput);
   });
 
-  // Check match by comparison with snapshot
-  test('should generate linked list from values 2', () => {
-    // Write your test here
+  test('should generate linked list from array [4, 5, 6]', () => {
+    const input = [4, 5, 6];
+    const linkedList = generateLinkedList(input);
+
+    expect(linkedList).toMatchSnapshot();
+  });
+
+  test('should generate an empty linked list from an empty array', () => {
+    const input: number[] = [];
+    const expectedOutput = {
+      value: null,
+      next: null,
+    };
+
+    expect(generateLinkedList(input)).toStrictEqual(expectedOutput);
   });
 });
